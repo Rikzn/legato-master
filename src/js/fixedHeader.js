3b6221353e47f1a1e84228e0769c86e8f0524418
Rikzn <41932726+Rikzn@users.noreply.github.com>
2021-04-29 17:46:23 +0300

    tes

--- src/js/fixedHeader.js
+++ src/js/fixedHeader.js
@@ -2,7 +2,7 @@ export default function fixedHeader() {
     const pageHeader = document.querySelector('.page-header');
     if (!pageHeader) return;
     window.addEventListener('scroll', () => {
-        if (window.pageYOffset > 900) {
+        if (window.pageYOffset > 800) {
             pageHeader.classList.add('fixed');
         } else {
             pageHeader.classList.remove('fixed');
